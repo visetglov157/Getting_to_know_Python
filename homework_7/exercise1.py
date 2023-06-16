119162cdf3b01777aea907d89668dd0437182b6f
1 <2>
2023-06-16 09:35:27 +0300

    задание 1 готово

--- homework_7/exercise1.py
+++ homework_7/exercise1.py
@@ -1,1 +1,17 @@
+def rhythm(str):
+    str = str.split()
+    list = []
+    for poem in str:
+        result = 0
+        for i in poem:
+            if i in 'аеёиоуыэюя':
+                result += 1
+        list.append(result)
+    return len(list) == list.count(list[0])
 
+print('Введите: пара-ра-рам рам-пам-папам па-ра-па-дам')
+str = input()
+if rhythm(str):
+    print('Парам пам-пам')
+else:
+    print('Пам парам')
