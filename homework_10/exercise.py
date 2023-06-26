93d1f37550f6e0b53920c2a9c1c7c36429ff403e
Vladislav <vladislav33lavrov@gmail.com>
2023-06-26 20:19:31 +0300

    Возможно не так

--- homework_10/exercise.py
+++ homework_10/exercise.py
@@ -1,1 +1,14 @@
+import random
+import pandas as pd
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+print(lst)
+print()
+data = pd.DataFrame({'whoAmI':lst})
+data.loc[data['whoAmI'] == 'robot', 'robot_group'] = '1'
+data.loc[data['whoAmI'] != 'robot', 'robot_group'] = '0'
+data.loc[data['whoAmI'] == 'human', 'human_group'] = '1'
+data.loc[data['whoAmI'] != 'human', 'human_group'] = '0'
 
+data.head(n=20) 
